c1c3384cbcba23c2380191caba44019c26109e94
tcrear <smileybear88@hotmail.com>
2021-11-21 21:11:05 -0600

    update changes to js test

---
+++ my-app/src/components/about-me.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloDiv() {
+  return (
+    <div>
+      <h1>Hello, I'm Tammie!</h1>
+      <h2>Full Stack Web Developer</h2>
+      <ul>
+        <li>Peas</li>
+        <li>Carrots</li>
+        <li>Love</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
