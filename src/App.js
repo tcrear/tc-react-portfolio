f7dffb2ed1c2e06438bdfc2838b0e94cc274abed
tcrear <smileybear88@hotmail.com>
2021-11-23 14:53:43 -0600

    updating files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+// import About Contact Home
+import About from './components/pages/About';
+
+function App() {
+  return (
+    <div>
+      <header>
+
+      </header>
+      <main>
+        <About />
+        {/* <Home />
+        <Contact /> */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
